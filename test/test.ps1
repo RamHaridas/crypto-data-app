a96d6f510d2d6df88f7e2f67ebb92d62c4883315
Ram Haridas <43177945+RamHaridas@users.noreply.github.com>
2025-01-13 22:50:20 +0530

    Update test.ps1

--- test/test.ps1
+++ test/test.ps1
@@ -1,11 +1,15 @@
 $resourceGroupName = "your-resource-group-name"
-$vmName = "your-vm-name"
+#$vmName = "your-vm-name"
+$vmNameList = @("vmnam1", "vmname2", "vmname3")
 
-# Get the VM
-$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+foreach($vmName in $vmNameList){
+  # Get the VM
+  $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+  
+  # Update the tag
+  $vm.Tags["app"] = "web"
+  
+  # Save the changes
+  Set-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Tag $vm.Tags
 
-# Update the tag
-$vm.Tags["app"] = "web"
-
-# Save the changes
-Set-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Tag $vm.Tags
+}
